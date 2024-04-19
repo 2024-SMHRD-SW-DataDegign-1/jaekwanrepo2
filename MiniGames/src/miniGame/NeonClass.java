7938173078bae6aba4c9aadc1ff1e95285231b13
smhrd <smhrd@DESKTOP-24L66D4>
2024-04-19 11:34:25 +0900

    오프닝 그림수정, 그림 퀴즈에 설명추가

--- MiniGames/src/miniGame/NeonClass.java
+++ MiniGames/src/miniGame/NeonClass.java
@@ -16,11 +16,16 @@ public class NeonClass {
 
 		int sum = 0;
 
-		System.out.println("넌센스 그림 퀴즈에 오신 것을 환영합니다!");
-	
+		String intro = "넌센스 그림퀴즈에 오신 걸 환영합니다.\n"
+					  +"당신의 센스를 확인해보세요 문제의 정답을 맞힐 수 있는 기회는 딱 3번 주어집니다.\n"
+					  +"한 문제당 만점은 30점이며 기회가 한번씩 깎일 때마다 힌트가 제공이 되며 점수가 -10점씩 차감됩니다.\n"
+					  +"Let's play~  ";
+		
+		slowPrint(intro, 50);
+		timeDelay(2);
+		
 		ArrayList<Integer> num = ranNum(16);
-		
-		
+		System.out.println("\n\n-------------------------------------------\n\n");
 		
 		for (int i = 0; i < 3; i++) {
 			int score = 30;
@@ -89,5 +94,17 @@ public class NeonClass {
 		}
 
 	}
+	public void slowPrint(String text, int delay) {
+        for (int i = 0; i < text.length(); i++) {
+            System.out.print(text.charAt(i));
+            try {
+                Thread.sleep(delay); // 지정한 시간만큼 대기
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(); // 마지막에 개행 출력
+    }
+	
 
 }
