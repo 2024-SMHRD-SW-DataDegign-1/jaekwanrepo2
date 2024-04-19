ca3ce64df8807b9fba8fe288041d219dbcae8c40
JHLEE6619 <167040658+JHLEE6619@users.noreply.github.com>
2024-04-19 19:08:52 +0900

    로그인 기능 수정, 메인메뉴 로그오프 추가

--- MiniGames/src/miniGame/DBCON.java
+++ MiniGames/src/miniGame/DBCON.java
@@ -4,28 +4,50 @@ import java.util.Scanner;
 
 public class DBCON {
 
-	public DBDTO loginMenu(int menu){
+	public DBDTO loginMenu(int menu) {
 		DBDTO user = null;
-		DBDAO dao= new DBDAO();
+		DBDAO dao = new DBDAO();
 		Scanner sc = new Scanner(System.in);
-		
+		String id = null;
+		String pw = null;
+		String name = null;
+
 		if (menu == 1) {
 			System.out.print("로그인할 ID 입력 : ");
-			String id = sc.next();
+			id = sc.next();
 			System.out.print("로그인할 PW 입력 : ");
-			String pw = sc.next();
+			pw = sc.next();
 
 			user = dao.loginUser(id, pw);
 
-			
-		}
-		else if (menu == 2) {
-			System.out.print("ID 입력 : ");
-			String id = sc.next();
-			System.out.print("PW 입력 : ");
-			String pw = sc.next();
-			System.out.print("이름 입력 : ");
-			String name = sc.next();
+		} else if (menu == 2) {
+			while (true) {
+				System.out.print("ID 입력 : ");
+				id = sc.next();
+				if (id.length() > 10 || id.length() < 4) {
+					System.out.println("아이디는 4~10글자 사이로 입력해주세요.");
+					continue;
+				}
+				break;
+			}
+			while (true) {
+				System.out.print("PW 입력 : ");
+				pw = sc.next();
+				if (pw.length() > 12 || pw.length() < 4) {
+					System.out.println("비밀번호는 4~12글자 사이로 입력해주세요.");
+					continue;
+				}
+				break;
+			}
+			while (true) {
+				System.out.print("이름 입력 : ");
+				name = sc.next();
+				if (name.length() > 5) {
+					System.out.println("이름은 5글자 이하로 입력해주세요.");
+					continue;
+				}
+				break;
+			}
 
 			int row = dao.insertUser(id, pw, name);
 			if (row > 0) {
@@ -35,21 +57,12 @@ public class DBCON {
 			}
 
 		}
+
 		else if (menu == 3) {
 			System.exit(0);
 		}
-		
-		
-		
-		
-		
+
 		return user;
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
--- MiniGames/src/miniGame/GameMain.java
+++ MiniGames/src/miniGame/GameMain.java
@@ -9,69 +9,70 @@ public class GameMain {
 		DBDTO user = null;
 		DBCON con = new DBCON();
 		image im = new image();
-		
+
 		im.printImage(".\\image\\오프닝.txt");
-		
+
 		while (true) {
-			System.out.println("--------------------------------------------------------------------------");
-			System.out.println("[1]로그인\t\t\t[2]회원가입\t\t[3]프로그램종료");
-			System.out.println("--------------------------------------------------------------------------");
-			int menu = sc.nextInt();
-			user = con.loginMenu(menu);
-			
-			if(menu==1) {
-				if (user==null) {
-					System.err.println("로그인 실패");
-				} else {
-					System.out.println("로그인 성공");
-					System.out.println();
-					System.out.println(user.getName() + "님 환영합니다.");
-					break;
+			while (true) {
+				System.out.println("--------------------------------------------------------------------------");
+				System.out.println("[1]로그인\t\t\t[2]회원가입\t\t[3]프로그램종료");
+				System.out.println("--------------------------------------------------------------------------");
+				int menu = sc.nextInt();
+				user = con.loginMenu(menu);
+
+				if (menu == 1) {
+					if (user == null) {
+						System.err.println("로그인 실패");
+					} else {
+						System.out.println("로그인 성공");
+						System.out.println();
+						System.out.println(user.getName() + "님 환영합니다.");
+						break;
+					}
+
 				}
 			}
-		
-		}
 
 //		String comPath = ".\\player\\";
-		while (true) {
-			System.out.println("--------------------------------------------------------------------------");
-			System.out.println("[1]노래맞추기 [2]틀린문법찾기 [3]넌센스그림퀴즈 [4]초성퀴즈 [5]랭킹보기 [0]게임종료");
-			System.out.println("--------------------------------------------------------------------------");
-			int num = sc.nextInt();
-
-			if (num == 1) {
-				MusicGameCon mp3 = new MusicGameCon();
-				mp3.playTest(user.getId());
-
-			} else if (num == 2) {
-				WrongCodeMain wcm = new WrongCodeMain();
-				wcm.wrongCodeMain(user);
-			} else if (num == 3) {/* 아현 : 넌센스 그림 맞추기 게임 */
-				NeonClass neon = new NeonClass();
-				neon.imageGame(user.getId());
-
-			} else if (num == 4) {
-				WordGame wordGame = new WordGame();
-	            wordGame.startGame(user.getId());
-			} else if (num == 5) {
-				RankDAO rank = new RankDAO();
+			while (true) {
 				System.out.println("--------------------------------------------------------------------------");
-				System.out.println("[1]노래맞추기 [2]틀린문법찾기 [3]넌센스그림퀴즈 [4]초성퀴즈 [0]뒤로가기");
+				System.out.println("[1]노래맞추기 [2]틀린문법찾기 [3]넌센스그림퀴즈 [4]초성퀴즈 [5]랭킹보기 [6]로그아웃 [0]게임종료");
 				System.out.println("--------------------------------------------------------------------------");
-				int numR = sc.nextInt();
-				if(numR ==0) {
-					continue;
-				}else {
-					rank.ranksys(numR);
+				int num = sc.nextInt();
+
+				if (num == 1) {
+					MusicGameCon mp3 = new MusicGameCon();
+					mp3.playTest(user.getId());
+
+				} else if (num == 2) {
+					WrongCodeMain wcm = new WrongCodeMain();
+					wcm.wrongCodeMain(user);
+				} else if (num == 3) {/* 아현 : 넌센스 그림 맞추기 게임 */
+					NeonClass neon = new NeonClass();
+					neon.imageGame(user.getId());
+
+				} else if (num == 4) {
+					WordGame wordGame = new WordGame();
+					wordGame.startGame(user.getId());
+				} else if (num == 5) {
+					RankDAO rank = new RankDAO();
+					System.out.println("--------------------------------------------------------------------------");
+					System.out.println("[1]노래맞추기 [2]틀린문법찾기 [3]넌센스그림퀴즈 [4]초성퀴즈 [0]뒤로가기");
+					System.out.println("--------------------------------------------------------------------------");
+					int numR = sc.nextInt();
+					if (numR == 0) {
+						continue;
+					} else {
+						rank.ranksys(numR);
+					}
+
+				} else if (num == 6) {
+					break;
+				} else if (num == 0) {
+					System.exit(0);
 				}
-				
 
-			} else if (num == 0) {
-				break;
 			}
-			
-			
-
 		}
 
 	}
