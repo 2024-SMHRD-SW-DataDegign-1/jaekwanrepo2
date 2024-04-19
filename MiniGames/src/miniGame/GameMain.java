2af394fe798b9a5f2d2b5382b884eae992597244
smhrd <smhrd@DESKTOP-P45J6HP>
2024-04-19 11:37:08 +0900

    메인추가

--- MiniGames/src/miniGame/GameMain.java
+++ MiniGames/src/miniGame/GameMain.java
@@ -46,7 +46,8 @@ public class GameMain {
 				neon.imageGame(user.getId());
 
 			} else if (num == 4) {
-
+				WordGame wordGame = new WordGame();
+	            wordGame.startGame();
 			} else if (num == 5) {
 				RankDAO rank = new RankDAO();
 				System.out.println("[1]노래맞추기 [2]틀린문법찾기 [3]넌센스그림퀴즈 [4]초성퀴즈 [0]뒤로가기");
